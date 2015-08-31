a43128e348507e2916aeb5e84965a252a5d05131
empadhyad <empadhyad@mycompany.com>
2015-08-31 14:34:11 +0000

    Added unit and integration test for report auditing
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@4066 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/AuditServiceBeanITest.java
@@ -0,0 +1,70 @@
+package eu.europa.ec.fisheries.uvms.reporting.service.bean;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import javax.ejb.EJB;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import eu.europa.ec.fisheries.uvms.common.AuditActionEnum;
+import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingException;
+
+@RunWith(Arquillian.class)
+@Transactional(TransactionMode.ROLLBACK)
+public class AuditServiceBeanITest {
+	
+	@EJB
+	AuditService auditService;
+	
+	@Deployment
+	  public static WebArchive createDeployment() {
+	  	WebArchive war = ShrinkWrap.create(WebArchive.class,"reporting-service.war").addPackages(true, "eu.europa.ec.fisheries.uvms.reporting.service")
+	              .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+	              .addAsManifestResource(new File( "src/test/resources/META-INF/jboss-deployment-structure.xml"))
+	              .addAsResource("config.properties")
+	              .addAsResource("logback.xml");
+	  	
+	      File[] libs = Maven.resolver().loadPomFromFile("pom.xml").importDependencies(ScopeType.COMPILE, ScopeType.RUNTIME, ScopeType.TEST, ScopeType.PROVIDED).resolve().withTransitivity().asFile();
+	      war = war.addAsLibraries(libs);
+
+	      System.out.println(war.toString(true)); 
+	      
+	      return war;
+	  }
+		
+		@Before
+		public void setUp() throws Exception {
+			assertNotNull("Audit not injected", auditService);
+		}
+
+		@After 
+		public void tearDown() throws Exception {
+		}
+		
+		@Test
+		@Transactional(TransactionMode.ROLLBACK)
+		public void testSendAuditReport() {	
+			try {
+				auditService.sendAuditReport(AuditActionEnum.CREATE, "123");
+				auditService.sendAuditReport(AuditActionEnum.MODIFY, "123");
+				auditService.sendAuditReport(AuditActionEnum.DELETE, "123");
+			} catch (ReportingException e) {
+				assertNull(e);
+			}
+		}		
+
+}
---
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/AuditServiceBeanTest.java
@@ -0,0 +1,43 @@
+package eu.europa.ec.fisheries.uvms.reporting.service.bean;
+
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import eu.europa.ec.fisheries.uvms.common.AuditActionEnum;
+import eu.europa.ec.fisheries.uvms.reporting.message.producer.bean.AuditProducerBean;
+import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuditServiceBeanTest {
+	
+	@Mock
+	AuditProducerBean auditProducerBean;
+	
+	@InjectMocks
+	AuditServiceBean auditServiceBean;
+	
+	@Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+	
+	@Test
+	public void testInterceptor() {
+		try {
+			auditServiceBean.sendAuditReport(AuditActionEnum.CREATE, "123");
+			auditServiceBean.sendAuditReport(AuditActionEnum.MODIFY, "123");
+			auditServiceBean.sendAuditReport(AuditActionEnum.DELETE, "123");
+		} catch (ReportingException e) {
+			assertNull(e);
+		}
+		
+	}
+
+}
