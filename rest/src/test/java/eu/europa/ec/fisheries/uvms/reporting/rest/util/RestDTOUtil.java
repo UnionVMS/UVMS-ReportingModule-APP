8a5dc82b1d7a1a920d71698caca7f1e4aecbaf64
Georgi Georgiev <georgi.georgiev@ordina.be>
2015-10-05 08:03:49 +0000

    just adding a new Rest TEST UTIL
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@5177 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ rest/src/test/java/eu/europa/ec/fisheries/uvms/reporting/rest/util/RestDTOUtil.java
@@ -0,0 +1,46 @@
+package eu.europa.ec.fisheries.uvms.reporting.rest.util;
+
+import eu.europa.ec.fisheries.uvms.reporting.model.VisibilityEnum;
+import eu.europa.ec.fisheries.uvms.reporting.service.dto.AuditDTO;
+import eu.europa.ec.fisheries.uvms.reporting.service.dto.ReportDTO;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+
+public class RestDTOUtil {
+
+    public static ReportDTO createReport(String name,
+                                         String createdBy,
+                                         Date date,
+                                         String description,
+                                         String outComponents,
+                                         String scopeName,
+                                         VisibilityEnum visibilityEnum) {
+        ReportDTO entity = ReportDTO.builder().build();
+
+        entity.setCreatedBy(createdBy);
+        entity.setAudit(new AuditDTO());
+        entity.getAudit().setCreatedOn(date);
+        entity.setDescription(description);
+        entity.setName(name);
+        entity.setOutComponents(outComponents);
+        entity.setExecutionLogs(new HashSet());
+        entity.setScopeName(scopeName);
+        entity.setVisibility(visibilityEnum);
+
+        return entity;
+    }
+
+    public static ReportDTO createRandomReport() {
+        Date currentDate = new Date();
+
+        return createReport("ReportName" + currentDate.getTime(),
+                "georgi",
+                currentDate,
+                "This is a report description created on " + new SimpleDateFormat("yyyy/MM/dd HH:mm").format(currentDate),
+                "OutComponents",
+                "123",
+                VisibilityEnum.SCOPE);
+    }
+}
