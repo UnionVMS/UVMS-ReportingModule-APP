05209748afeefa5b66185b6c3899e3c284e4d461
emrinalgr <emrinalgr@mycompany.com>
2015-10-09 18:26:09 +0000

    common DateUtils
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@5387 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/reporting/rest/json/CustomDateSerializer.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/reporting/rest/json/CustomDateSerializer.java
@@ -1,27 +1,18 @@
 package eu.europa.ec.fisheries.uvms.reporting.rest.json;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import eu.europa.ec.fisheries.uvms.common.DateUtils;
+
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
+public class CustomDateSerializer extends JsonSerializer<Date> {
 
-
-
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.SerializerProvider;
-
-import eu.europa.ec.fisheries.uvms.reporting.rest.constants.RestConstants;
-
-
-
-public class CustomDateSerializer extends com.fasterxml.jackson.databind.JsonSerializer<Date> {
-	 
-	private static SimpleDateFormat formatter = new SimpleDateFormat(RestConstants.DATE_TIME_UI_FORMAT);
-		 
     @Override
     public void serialize (Date value, JsonGenerator gen, SerializerProvider arg2)
-      throws IOException, JsonProcessingException {
-        gen.writeString(formatter.format(value));
+      throws IOException {
+        gen.writeString(DateUtils.UI_FORMATTER.print(value.getTime()));
     }
 }
