ac3976922071b844448ccec925695ee0b34b0eb7
emrinalgr <emrinalgr@mycompany.com>
2015-10-11 17:03:27 +0000

    re-enable unit test
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@5394 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/test/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/VmsServiceBeanTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/VmsServiceBeanTest.java
@@ -1,18 +1,13 @@
 package eu.europa.ec.fisheries.uvms.reporting.service.bean;
 
 import eu.europa.ec.fisheries.uvms.message.MessageException;
-import eu.europa.ec.fisheries.uvms.reporting.message.service.MovementMessageService;
 import eu.europa.ec.fisheries.uvms.movement.model.exception.ModelMapperException;
+import eu.europa.ec.fisheries.uvms.reporting.message.service.MovementMessageService;
 import eu.europa.ec.fisheries.uvms.reporting.message.service.VesselMessageService;
-import eu.europa.ec.fisheries.uvms.reporting.message.service.MovementMessageServiceBean;
 import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingServiceException;
-import eu.europa.ec.fisheries.uvms.reporting.message.service.VesselMessageServiceBean;
 import eu.europa.ec.fisheries.uvms.reporting.service.entities.Filter;
-import eu.europa.ec.fisheries.uvms.reporting.service.dao.ReportDAO;
 import eu.europa.ec.fisheries.uvms.reporting.service.entities.Report;
 import eu.europa.ec.fisheries.uvms.vessel.model.exception.VesselModelMapperException;
-import eu.europa.ec.fisheries.uvms.reporting.service.entities.VesselGroupFilter;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.unitils.UnitilsJUnit4;
 import org.unitils.inject.annotation.InjectIntoByType;
@@ -44,28 +39,24 @@ public class VmsServiceBeanTest extends UnitilsJUnit4 {
     private Mock<Report> report;
 
     @Test
-    @Ignore
     public void testGetVmsDataByReportId() throws VesselModelMapperException, MessageException, ModelMapperException, JMSException, ReportingServiceException {
 
         Set<Filter> filterSet = new HashSet<>();
         filterSet.add(VesselFilterBuilder().guid("1234").build());
 
         report.returns(filterSet).getFilters();
-        //FIXME ... the commented lines
-      //  vesselModule.returns(new ArrayList<>()).getVesselsByVesselListQuery(null, "usernam");
-        //repository.returns(report.getMock()).findReportByReportId(null);
+        vesselModule.returns(new ArrayList<>()).getVesselsByVesselListQuery(null);
+        repository.returns(report.getMock()).findReportByReportId(null, "userName", null);
 
-        //service.getVmsDataByReportId("test", null);
+        service.getVmsDataByReportId("userName", "scope",  null);
 
         vesselModule.assertInvoked().getVesselsByVesselListQuery(null);
         vesselModule.assertNotInvoked().getVesselsByVesselGroups(null);
         movementModule.assertInvoked().getMovementMap(null);
-        report.assertInvoked().updateExecutionLog("test");
+        report.assertInvoked().updateExecutionLog("userName");
     }
 
-    //FIXME fix the test and the commented stuff
     @Test
-    @Ignore
     public void testGetVmsDataByReportIdWithVesselGroupFilters() throws Exception {
 
         Set<Filter> filterSet = new HashSet<>();
@@ -73,9 +64,8 @@ public class VmsServiceBeanTest extends UnitilsJUnit4 {
 
         report.returns(filterSet).getFilters();
         vesselModule.returns(new ArrayList<>()).getVesselsByVesselGroups(null);
-//        repository.returns(report).findReportByReportId(null);
-//
-//        service.getVmsDataByReportId("test", null);
+        repository.returns(report).findReportByReportId(null, "test", null);
+        service.getVmsDataByReportId("test", null, null);
 
         vesselModule.assertNotInvoked().getVesselsByVesselListQuery(null);
         vesselModule.assertInvoked().getVesselsByVesselGroups(null);
@@ -85,9 +75,7 @@ public class VmsServiceBeanTest extends UnitilsJUnit4 {
     }
 
     @Test(expected = ReportingServiceException.class)
-    @Ignore
-    //FIXME fix the test and the commented stuff
     public void testReportNull() throws ReportingServiceException {
-//        service.getVmsDataByReportId("test", null);
+        service.getVmsDataByReportId("test", null, null);
     }
 }
