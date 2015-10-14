c78c79e35c5938574e941570729d491a0572c8ba
empadhyad <empadhyad@mycompany.com>
2015-10-14 15:21:03 +0000

    UNIONVMS-775
    JMS queue producer for filter areas
    
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@5562 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- message/src/main/java/eu/europa/ec/fisheries/uvms/reporting/message/mapper/SpatialMessageMapper.java
+++
@@ -1,4 +0,0 @@
-package eu.europa.ec.fisheries.uvms.reporting.message.mapper;
-
-public interface SpatialMessageMapper {
-}
--- message/src/main/java/eu/europa/ec/fisheries/uvms/reporting/message/mapper/impl/SpatialMessageMapperImpl.java
+++
@@ -1,6 +0,0 @@
-package eu.europa.ec.fisheries.uvms.reporting.message.mapper.impl;
-
-import eu.europa.ec.fisheries.uvms.reporting.message.mapper.SpatialMessageMapper;
-
-public class SpatialMessageMapperImpl implements SpatialMessageMapper {
-}
