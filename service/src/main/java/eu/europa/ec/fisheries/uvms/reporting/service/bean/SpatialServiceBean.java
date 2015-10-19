510119647ca3760e2c882ee381bfbc63d5fa4859
empadhyad <empadhyad@mycompany.com>
2015-10-19 10:08:46 +0000

    UNIONVMS-775
    JMS queue producer for filter areas
    
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@5712 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/SpatialServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/SpatialServiceBean.java
@@ -1,31 +1,43 @@
 package eu.europa.ec.fisheries.uvms.reporting.service.bean;
 
+import java.util.Collections;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
 
 import org.apache.commons.lang3.NotImplementedException;
 
 import eu.europa.ec.fisheries.uvms.message.MessageException;
-import eu.europa.ec.fisheries.uvms.reporting.message.service.SpatialMessageService;
+import eu.europa.ec.fisheries.uvms.reporting.message.service.SpatialConsumerBean;
+import eu.europa.ec.fisheries.uvms.reporting.message.service.SpatialProducerBean;
 import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingServiceException;
 import eu.europa.ec.fisheries.uvms.spatial.model.exception.SpatialModelMapperException;
+import eu.europa.ec.fisheries.uvms.spatial.model.mapper.SpatialModuleRequestMapper;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaIdentifierType;
 
 @Stateless
 @Local(SpatialService.class)
 public class SpatialServiceBean implements SpatialService {
-
+	
 	@EJB
-	SpatialMessageService spatialMessageService;
+	private SpatialProducerBean spatialProducerBean;
+	
+	@EJB
+	private SpatialConsumerBean spatialConsumerBean;
 	
 	@Override
 	public String getFilterArea(List<AreaIdentifierType> userAreas) throws ReportingServiceException {
 		try {
-			return spatialMessageService.getFilteredAreas(userAreas);
+			SpatialModuleRequestMapper requestMapper = new SpatialModuleRequestMapper();
+			String requestMsg = requestMapper.mapToFilterAreaSpatialRequest(Collections.<AreaIdentifierType>emptyList(), userAreas);
+			String correlationId = spatialProducerBean.sendModuleMessage(requestMsg, spatialConsumerBean.getDestination());
+			Message message = spatialConsumerBean.getMessage(correlationId, TextMessage.class);
+			return getText(message);
 		} catch (SpatialModelMapperException | MessageException | JMSException e) {
 			throw new ReportingServiceException(e);
 		}
@@ -34,6 +46,13 @@ public class SpatialServiceBean implements SpatialService {
 	@Override
 	public String getFilterArea(List<AreaIdentifierType> scopeAreas, List<AreaIdentifierType> userAreas) throws ReportingServiceException {
 		throw new NotImplementedException("Not implemented");
+	}
+	
+	private String getText(Message message) throws JMSException {
+		if(message instanceof TextMessage) {
+			return ((TextMessage)message).getText();
+		}
+		return null;
 	}
 
 }
