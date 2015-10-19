00622dd3fc46e6eb5e9733684403862e79dd8f78
empadhyad <empadhyad@mycompany.com>
2015-10-19 10:02:43 +0000

    UNIONVMS-775
    JMS queue producer for filter areas
    
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@5711 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/reporting/message/service/SpatialProducerBean.java
@@ -0,0 +1,23 @@
+package eu.europa.ec.fisheries.uvms.reporting.message.service;
+
+import javax.annotation.Resource;
+import javax.ejb.Local;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.jms.Destination;
+
+import eu.europa.ec.fisheries.uvms.message.AbstractProducer;
+import eu.europa.ec.fisheries.uvms.message.MessageConstants;
+
+@Stateless
+@Local
+public class SpatialProducerBean extends AbstractProducer {
+	
+	@Resource(mappedName = MessageConstants.QUEUE_MODULE_SPATIAL)
+    private Destination destination;
+
+	@Override
+	protected Destination getDestination() {
+		return destination;
+	}
+}
