91fd15b3d4016fd0feac603b04aedfa26747e004
emrinalgr <emrinalgr@mycompany.com>
2015-10-22 08:39:24 +0000

    UNIONVMS-747 vessel vesselgroup + some simplification in vmsservicebean
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@5842 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/reporting/service/entity/ReportTest.java
@@ -0,0 +1,36 @@
+package eu.europa.ec.fisheries.uvms.reporting.service.entity;
+
+import eu.europa.ec.fisheries.uvms.reporting.service.entities.Report;
+import org.junit.Test;
+import org.unitils.UnitilsJUnit4;
+import org.unitils.inject.annotation.TestedObject;
+import org.unitils.mock.PartialMock;
+import org.unitils.mock.annotation.Dummy;
+
+public class ReportTest extends UnitilsJUnit4 {
+
+    @TestedObject
+    private PartialMock<Report> report;
+
+    @Dummy
+    private Report dummy;
+
+    @Test
+    public void testMerge(){
+
+        report.getMock().merge(dummy);
+
+        report.assertInvoked().setId(null);
+        report.assertInvoked().setName(null);
+        report.assertInvoked().setDescription(null);
+        report.assertInvoked().setWithMap(null);
+        report.assertInvoked().setIsDeleted(null);
+        report.assertInvoked().setDeletedBy(null);
+        report.assertInvoked().setVisibility(null);
+
+        report.assertNotInvoked().setScopeName(null);
+        report.assertNotInvoked().setCreatedBy(null);
+        report.assertNotInvoked().setExecutionLogs(null);
+
+    }
+}
