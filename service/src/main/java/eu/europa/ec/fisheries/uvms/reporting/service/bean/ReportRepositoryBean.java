7eb511142b84b8464c15ba94a7422cccad83ee1e
emrinalgr <emrinalgr@mycompany.com>
2015-11-10 11:26:48 +0000

    typo
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@6591 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/ReportRepositoryBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/ReportRepositoryBean.java
@@ -16,8 +16,6 @@ import org.apache.commons.collections4.CollectionUtils;
 import javax.annotation.PostConstruct;
 import javax.ejb.Local;
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -41,7 +39,7 @@ public class ReportRepositoryBean implements ReportRepository {
     private EntityManager em;
 
     @PostConstruct
-    public void postContruct(){
+    public void postConstruct(){
         reportDAO = new ReportDAO(em);
         filterDAO = new FilterDAO(em);
         executionLogDAO = new ExecutionLogDAO(em);
