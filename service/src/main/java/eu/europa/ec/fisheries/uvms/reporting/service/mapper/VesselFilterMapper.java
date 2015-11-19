d4c936b395516429160ccffb187f56bf7309f540
emrinalgr <emrinalgr@mycompany.com>
2015-11-19 12:38:40 +0000

    augment vesselFilterMapper
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@6814 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/mapper/VesselFilterMapper.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/mapper/VesselFilterMapper.java
@@ -1,9 +1,12 @@
 package eu.europa.ec.fisheries.uvms.reporting.service.mapper;
 
+import eu.europa.ec.fisheries.schema.movement.search.v1.ListCriteria;
 import eu.europa.ec.fisheries.uvms.reporting.service.dto.VesselFilterDTO;
 import eu.europa.ec.fisheries.uvms.reporting.service.entities.VesselFilter;
+import eu.europa.ec.fisheries.wsdl.vessel.types.VesselListCriteriaPair;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = ObjectFactory.class)
@@ -14,5 +17,17 @@ public interface VesselFilterMapper {
     VesselFilterDTO vesselFilterToVesselFilterDTO(VesselFilter vesselFilter);
 
     VesselFilter vesselFilterDTOToVesselFilter(VesselFilterDTO vesselFilterDTO);
+
+    @Mappings({
+            @Mapping(source = "guid", target = "value"),
+            @Mapping(constant = "GUID", target = "key"),
+    })
+    VesselListCriteriaPair vesselFilterToVesselListCriteriaPair(VesselFilter vesselFilter);
+
+    @Mappings({
+            @Mapping(source = "guid", target = "value"),
+            @Mapping(constant = "CONNECT_ID", target = "key"),
+    })
+    ListCriteria vesselFilterToListCriteria(VesselFilter vesselFilter);
 
 }
