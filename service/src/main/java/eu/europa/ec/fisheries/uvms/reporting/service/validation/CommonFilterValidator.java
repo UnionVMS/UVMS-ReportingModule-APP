2cbf34ee75af0d1250ca6a2be4f582f4e2a60b9e
emrinalgr <emrinalgr@mycompany.com>
2015-11-19 12:42:43 +0000

    introducing commonfilter validator
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@6816 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/validation/CommonFilterValidator.java
@@ -0,0 +1,36 @@
+package eu.europa.ec.fisheries.uvms.reporting.service.validation;
+
+import eu.europa.ec.fisheries.uvms.reporting.service.entities.CommonFilter;
+import eu.europa.ec.fisheries.uvms.reporting.service.entities.PositionSelector;
+import eu.europa.ec.fisheries.uvms.reporting.service.entities.Selector;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CommonFilterValidator implements ConstraintValidator<CommonFilterIsValid, CommonFilter> {
+
+    @Override
+    public void initialize(CommonFilterIsValid commonFilterIsValid) {
+    }
+
+    @Override
+    public boolean isValid(CommonFilter commonFilter, ConstraintValidatorContext constraintValidatorContext) {
+
+        boolean valid = true;
+
+        PositionSelector positionSelector = commonFilter.getPositionSelector();
+
+        if (positionSelector != null && Selector.all.equals(positionSelector.getSelector())){
+            valid = positionSelector.getPosition() == null &&
+                    positionSelector.getValue() == null;
+        }
+
+        else if (positionSelector != null && Selector.last.equals(positionSelector.getSelector())){
+            valid = commonFilter.getDateRange() == null ||
+                    commonFilter.getDateRange().getEndDate() == null &&
+                    commonFilter.getDateRange().getStartDate() == null;
+        }
+
+        return valid;
+    }
+}
