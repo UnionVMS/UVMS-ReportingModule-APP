4e3f0a5fa5a66b7b8879d3a0a681c5096f55f8c1
emrinalgr <emrinalgr@mycompany.com>
2015-11-19 12:39:08 +0000

    augment vesselGroupFilterMapper
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@6815 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/mapper/VesselGroupFilterMapper.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/mapper/VesselGroupFilterMapper.java
@@ -1,10 +1,14 @@
 package eu.europa.ec.fisheries.uvms.reporting.service.mapper;
 
 import eu.europa.ec.fisheries.uvms.reporting.service.dto.VesselGroupFilterDTO;
+import eu.europa.ec.fisheries.uvms.reporting.service.entities.VesselFilter;
 import eu.europa.ec.fisheries.uvms.reporting.service.entities.VesselGroupFilter;
+import eu.europa.ec.fisheries.wsdl.vessel.group.VesselGroup;
+import eu.europa.ec.fisheries.wsdl.vessel.types.VesselListCriteriaPair;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = ObjectFactory.class)
@@ -16,4 +20,9 @@ public interface VesselGroupFilterMapper {
 
     VesselGroupFilter vesselGroupFilterDTOToVesselGroupFilter(VesselGroupFilterDTO vesselGroupFilterDTO);
 
+    @Mappings({
+            @Mapping(constant = "true", target = "dynamic"),
+            @Mapping(source = "userName", target = "user"),
+    })
+    VesselGroup vesselGroupFilterToVesselGroup(VesselGroupFilter vesselGroupFilter);
 }
