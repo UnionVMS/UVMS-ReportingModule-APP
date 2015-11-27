7707567a03e59db50cec0da4a7e98457274e93b3
emrinalgr <emrinalgr@mycompany.com>
2015-11-27 12:53:46 +0000

    reformat
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@7070 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/VmsServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/VmsServiceBean.java
@@ -10,7 +10,6 @@ import eu.europa.ec.fisheries.uvms.reporting.service.entities.Report;
 import eu.europa.ec.fisheries.uvms.reporting.service.mapper.FilterProcessor;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaIdentifierType;
 import eu.europa.ec.fisheries.wsdl.vessel.types.Vessel;
-
 import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Stateless;
@@ -31,74 +30,105 @@ public class VmsServiceBean implements VmsService {
     private ReportRepository repository;
 
     @EJB
-    private VesselServiceBean vessel;
+    private VesselServiceBean vesselModule;
 
     @EJB
-    private MovementServiceBean movement;
+    private MovementServiceBean movementModule;
     
     @EJB
-    private SpatialService spatialService;
+    private SpatialService spatialModule;
 
     @Override
     @Transactional
     public VmsDTO getVmsDataByReportId(final String username, final String scopeName, final Long id) throws ReportingServiceException {
 
+        Report reportByReportId = repository.findReportByReportId(id, username, scopeName);
+
+        if (reportByReportId == null) {
+
+            throw new ReportingServiceException("No report found with id " + id);
+
+        }
+
         VmsDTO vmsDto;
-        Map<String, Vessel> vesselMap;
-        Collection<MovementMapResponseType> movementMap;
-        Map<String, MovementMapResponseType> responseTypeMap;
 
         try {
 
-            Report reportByReportId = repository.findReportByReportId(id, username, scopeName);
-            validate(id, reportByReportId);
+            Map<String, Vessel> vesselMap;
 
             FilterProcessor processor = new FilterProcessor(reportByReportId.getFilters());
-            addAreaCriteriaToProcessor(processor);            
+
+            addAreaCriteriaToProcessor(processor);
+
+            Collection<MovementMapResponseType> movementMap;
+
+            Map<String, MovementMapResponseType> responseTypeMap;
 
             if (processor.hasVesselsOrVesselGroups()) {
-                vesselMap = vessel.getVesselMap(processor);
+
+                vesselMap = vesselModule.getVesselMap(processor);
+
                 processor.getMovementListCriteria().addAll(ExtMovementMessageMapper.movementListCriteria(vesselMap.keySet()));
-                movementMap = movement.getMovement(processor);
+
+                movementMap = movementModule.getMovement(processor);
+
             } else {
-                responseTypeMap = movement.getMovementMap(processor);
+
+                responseTypeMap = movementModule.getMovementMap(processor);
+
                 Set<String> vesselGuids = responseTypeMap.keySet();
+
                 movementMap = responseTypeMap.values();
+
                 processor.getVesselListCriteriaPairs().addAll(ExtVesselMessageMapper.vesselCriteria(vesselGuids));
-                vesselMap = vessel.getVesselMap(processor);
+
+                vesselMap = vesselModule.getVesselMap(processor);
             }
 
             vmsDto = new VmsDTO(vesselMap, movementMap);
+
             reportByReportId.updateExecutionLog(username);
 
         } catch (ProcessorException e) {
+
             throw new ReportingServiceException("Error while processing reporting filters.", e);
+
         }
 
         return vmsDto;
+
     }
 
     private void addAreaCriteriaToProcessor(FilterProcessor processor) throws ReportingServiceException {
-    	if (isNotEmpty(processor.getAreaIdentifierList())) {
-    		String areaWkt = getFilterArea(processor.getAreaIdentifierList());
+
+        final Set<AreaIdentifierType> areaIdentifierList = processor.getAreaIdentifierList();
+
+        if (isNotEmpty(areaIdentifierList)) {
+
+    		String areaWkt = getFilterArea(areaIdentifierList);
+
         	processor.addAreaCriteria(areaWkt);
-    	}    	
+
+    	}
+
     }
     
 	private String getFilterArea(Set<AreaIdentifierType> areaIdentifierList) throws ReportingServiceException {
-		try {
-            List<AreaIdentifierType> areaIdentifierTypeList = new ArrayList<>();
-            areaIdentifierTypeList.addAll(areaIdentifierList);
-			return spatialService.getFilterArea(areaIdentifierTypeList);
-		} catch (ReportingServiceException e) {
-			throw new ReportingServiceException("Exception during retrieving filter area", e);
-		}
-	}
 
-    private void validate(Long id, Report reportByReportId) throws ReportingServiceException {
-        if (reportByReportId == null) {
-            throw new ReportingServiceException("No report found with id " + id);
-        }
-    }
+		try {
+
+            List<AreaIdentifierType> areaIdentifierTypeList = new ArrayList<>();
+
+            areaIdentifierTypeList.addAll(areaIdentifierList);
+
+			return spatialModule.getFilterArea(areaIdentifierTypeList);
+
+		} catch (ReportingServiceException e) {
+
+			throw new ReportingServiceException("Exception during retrieving filter area", e);
+
+		}
+
+	}
 
 }
