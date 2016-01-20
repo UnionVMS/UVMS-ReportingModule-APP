1e8d833a5c04b3fdeaed6b4418dbfd7426362171
emkopycmi <emkopycmi@mycompany.com>
2016-01-20 13:38:11 +0000

    #BUG - Fixed NPE when track contains only the coordinates of one (the same) point
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Reporting/APP@7821 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/dto/TrackDTO.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/dto/TrackDTO.java
@@ -1,8 +1,5 @@
 package eu.europa.ec.fisheries.uvms.reporting.service.dto;
 
-import static javax.measure.unit.NonSI.KNOT;
-import static javax.measure.unit.NonSI.NAUTICAL_MILE;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.vividsolutions.jts.geom.Coordinate;
@@ -15,19 +12,28 @@ import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingServiceExc
 import eu.europa.ec.fisheries.uvms.reporting.service.util.GeometryUtil;
 import eu.europa.ec.fisheries.wsdl.asset.types.Asset;
 import lombok.Setter;
+
 import javax.measure.converter.UnitConverter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.measure.unit.NonSI.KNOT;
+import static javax.measure.unit.NonSI.NAUTICAL_MILE;
+
 public class TrackDTO {
 
-    @JsonIgnore private MovementTrack track;
-    @JsonIgnore private Geometry geometry;
-    @JsonUnwrapped private AssetDTO asset;
+    @JsonIgnore
+    private MovementTrack track;
+    @JsonIgnore
+    private Geometry geometry;
+    @JsonUnwrapped
+    private AssetDTO asset;
     private List<Double> nearestPoint;
     private List<Double> extent;
-    @Setter private UnitConverter velocityConverter = KNOT.getConverterTo(KNOT);
-    @Setter private UnitConverter lengthConverter = NAUTICAL_MILE.getConverterTo(NAUTICAL_MILE);
+    @Setter
+    private UnitConverter velocityConverter = KNOT.getConverterTo(KNOT);
+    @Setter
+    private UnitConverter lengthConverter = NAUTICAL_MILE.getConverterTo(NAUTICAL_MILE);
 
     public TrackDTO(MovementTrack track, Asset asset) throws ReportingServiceException, ParseException {
         this.track = track;
@@ -40,26 +46,39 @@ public class TrackDTO {
     public TrackDTO(MovementTrack track, Asset asset, DisplayFormat format) throws ReportingServiceException, ParseException {
         this(track, asset);
 
-        if (format != null){
-            lengthConverter = format.getLengthType().getConverter();
-            velocityConverter = format.getVelocityType().getConverter();
+        if (format != null) {
+            if (format.getLengthType() != null) {
+                lengthConverter = format.getLengthType().getConverter();
+            }
+            if (format.getVelocityType() != null) {
+                velocityConverter = format.getVelocityType().getConverter();
+            }
         }
 
     }
 
     private void computerNearestPoint() {
         nearestPoint = new ArrayList<>();
-        if (geometry !=null && geometry.getCentroid() != null){
-            DistanceOp distanceOp = new DistanceOp(geometry.getCentroid(), geometry);
-            Coordinate[] nearestPoints = distanceOp.nearestPoints();
-            nearestPoint.add(nearestPoints[0].x);
-            nearestPoint.add(nearestPoints[0].y);
+        if (geometry != null && geometry.getCentroid() != null) {
+            if (geometry.isValid()) {
+                DistanceOp distanceOp = new DistanceOp(geometry.getCentroid(), geometry);
+                Coordinate[] nearestPoints = distanceOp.nearestPoints();
+                nearestPoint.add(nearestPoints[0].x);
+                nearestPoint.add(nearestPoints[0].y);
+            } else if (geometryContainsOnlyOnePoint()) {
+                nearestPoint.add(geometry.getCoordinate().x);
+                nearestPoint.add(geometry.getCoordinate().y);
+            }
         }
+    }
+
+    private boolean geometryContainsOnlyOnePoint() {
+        return geometry.getLength() == 0;
     }
 
     private void computeEnvelope() {
         extent = new ArrayList<>();
-        if (geometry != null && geometry.getEnvelopeInternal() != null){
+        if (geometry != null && geometry.getEnvelopeInternal() != null) {
             Envelope internal = geometry.getEnvelopeInternal();
             extent.add(internal.getMinX());
             extent.add(internal.getMinY());
@@ -95,9 +114,9 @@ public class TrackDTO {
         return track.getDuration();
     }
 
-    public String getId(){
+    public String getId() {
 
-        return  track.getId();
+        return track.getId();
 
     }
 }
