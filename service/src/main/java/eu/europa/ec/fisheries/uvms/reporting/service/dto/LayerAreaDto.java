04ed247cb1934cecab5f0d3145aeca7024502c54
empadhyad <empadhyad@mycompany.com>
2016-03-10 08:18:20 +0000

    UNIONVMS-1238
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Reporting/APP@9770 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/dto/LayerAreaDto.java
@@ -0,0 +1,78 @@
+package eu.europa.ec.fisheries.uvms.reporting.service.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import eu.europa.ec.fisheries.uvms.reporting.service.util.AreaTypeEnum;
+
+/**
+ * Created by padhyad on 1/11/2016.
+ */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class LayerAreaDto extends LayersDto {
+
+    @JsonProperty("areaType")
+    private AreaTypeEnum areaType;
+
+    @JsonProperty("gid")
+    private Long gid;
+
+    @JsonProperty("areaName")
+    private String areaName;
+
+    @JsonProperty("desc")
+    private String areaDesc;
+
+    @JsonProperty("areaGroupName")
+    private String areaGroupName;
+
+    @JsonProperty("areaType")
+    public AreaTypeEnum getAreaType() {
+        return areaType;
+    }
+
+    @JsonProperty("areaType")
+    public void setAreaType(AreaTypeEnum areaType) {
+        this.areaType = areaType;
+    }
+
+    @JsonProperty("gid")
+    public Long getGid() {
+        return gid;
+    }
+
+    @JsonProperty("gid")
+    public void setGid(Long gid) {
+        this.gid = gid;
+    }
+
+    @JsonProperty("areaName")
+    public String getAreaName() {
+        return areaName;
+    }
+
+    @JsonProperty("areaName")
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    @JsonProperty("desc")
+    public String getAreaDesc() {
+        return areaDesc;
+    }
+
+    @JsonProperty("desc")
+    public void setAreaDesc(String areaDesc) {
+        this.areaDesc = areaDesc;
+    }
+
+    @JsonProperty("areaGroupName")
+    public String getAreaGroupName() {
+        return areaGroupName;
+    }
+
+    @JsonProperty("areaGroupName")
+    public void setAreaGroupName(String areaGroupName) {
+        this.areaGroupName = areaGroupName;
+    }
+
+}
---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/dto/LayersDto.java
@@ -0,0 +1,85 @@
+package eu.europa.ec.fisheries.uvms.reporting.service.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by padhyad on 11/25/2015.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LayersDto implements Comparable<LayersDto> {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("serviceLayerId")
+    private String serviceLayerId;
+
+    @JsonProperty("subType")
+    private String subType;
+
+    @JsonProperty("order")
+    private Long order;
+
+    public LayersDto() {}
+
+    public LayersDto(String name, String serviceLayerId, String subType) {
+        this.name = name;
+        this.serviceLayerId = serviceLayerId;
+        this.subType = subType;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("serviceLayerId")
+    public String getServiceLayerId() {
+        return serviceLayerId;
+    }
+
+    @JsonProperty("serviceLayerId")
+    public void setServiceLayerId(String serviceLayerId) {
+        this.serviceLayerId = serviceLayerId;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [type = " + name + ", serviceLayerId = " + serviceLayerId + "]";
+    }
+
+    @JsonProperty("subType")
+    public String getSubType() {
+        return subType;
+    }
+
+    @JsonProperty("subType")
+    public void setSubType(String subType) {
+        this.subType = subType;
+    }
+
+    @JsonProperty("order")
+    public Long getOrder() {
+        return order;
+    }
+
+    @JsonProperty("order")
+    public void setOrder(Long order) {
+        this.order = order;
+    }
+
+    @Override
+    public int compareTo(LayersDto layersDto) {
+        if (this.getOrder() == null || layersDto.getOrder() == null) {
+            return 1;
+        }
+        return Long.compare(this.getOrder(), layersDto.getOrder());
+    }
+}
+
