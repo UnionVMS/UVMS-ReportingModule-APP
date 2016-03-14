23012d2ab4cf1fc4956b29a2c1f8b33a5fa66460
Georgi Georgiev <georgi.georgiev@ordina.be>
2016-03-14 10:00:49 +0000

    error handling on JMS error coming from Movement
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Reporting/APP@9841 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/MovementServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/MovementServiceBean.java
@@ -1,6 +1,7 @@
 package eu.europa.ec.fisheries.uvms.reporting.service.bean;
 
 import eu.europa.ec.fisheries.schema.movement.search.v1.MovementMapResponseType;
+import eu.europa.ec.fisheries.uvms.message.AbstractJAXBMarshaller;
 import eu.europa.ec.fisheries.uvms.message.MessageException;
 import eu.europa.ec.fisheries.uvms.movement.model.exception.ModelMapperException;
 import eu.europa.ec.fisheries.uvms.reporting.message.mapper.ExtMovementMessageMapper;
@@ -8,17 +9,22 @@ import eu.europa.ec.fisheries.uvms.reporting.message.service.MovementModuleSende
 import eu.europa.ec.fisheries.uvms.reporting.message.service.ReportingModuleReceiverBean;
 import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingServiceException;
 import eu.europa.ec.fisheries.uvms.reporting.service.mapper.FilterProcessor;
+import eu.europa.ec.fisheries.wsdl.user.types.UserFault;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
+import javax.xml.bind.JAXBException;
 import java.util.List;
 import java.util.Map;
 
 @Stateless
-public class MovementServiceBean {
+@Slf4j
+public class MovementServiceBean  extends AbstractJAXBMarshaller {
 
     @EJB
     private MovementModuleSenderBean movementSender;
@@ -35,7 +41,7 @@ public class MovementServiceBean {
 
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public List<MovementMapResponseType> getMovement(FilterProcessor processor) throws ReportingServiceException {
-
+        log.debug("getMovement({})", processor.toString());
         return getMovementMapResponseTypes(processor);
     }
 
@@ -46,10 +52,28 @@ public class MovementServiceBean {
             String request = ExtMovementMessageMapper.mapToGetMovementMapByQueryRequest(processor.toMovementQuery());
             String moduleMessage = movementSender.sendModuleMessage(request, receiver.getDestination());
             TextMessage response = receiver.getMessage(moduleMessage, TextMessage.class);
-            return ExtMovementMessageMapper.mapToMovementMapResponse(response);
+            if (response != null && !isUserFault(response)) {
+                return ExtMovementMessageMapper.mapToMovementMapResponse(response);
+            } else {
+                throw new ReportingServiceException("FAILED TO GET DATA FROM MOVEMENT");
+            }
 
         } catch (ModelMapperException | JMSException | MessageException e) {
             throw new ReportingServiceException("FAILED TO GET DATA FROM MOVEMENT", e);
         }
+    }
+
+    private boolean isUserFault(TextMessage message) {
+        boolean isErrorResponse = false;
+
+        try {
+            UserFault userFault = unmarshallTextMessage(message, UserFault.class);
+            log.error("UserFault error JMS message received with text: " + userFault.getFault());
+            isErrorResponse = true;
+        } catch (JAXBException | JMSException e) {
+            //do nothing  since it's not a UserFault
+        }
+
+        return isErrorResponse;
     }
 }
