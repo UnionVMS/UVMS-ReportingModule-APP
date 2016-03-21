30873acaca2e8ae2669d024a60cd3875dd53073b
EMRINALGR <EMRINALGR@mycompany.com>
2016-03-21 14:52:52 +0000

    reformatting code
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Reporting/APP@9977 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/VmsServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/VmsServiceBean.java
@@ -16,7 +16,6 @@ import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaIdentifierType;
 import eu.europa.ec.fisheries.wsdl.asset.types.Asset;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.helpers.MessageFormatter;
-
 import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Stateless;
@@ -33,24 +32,12 @@ import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 @Slf4j
 public class VmsServiceBean implements VmsService {
 
-    @EJB
-    private ReportRepository repository;
-
-    @EJB
-    private AuditService auditService;
-
-
-    @EJB
-    private AssetServiceBean assetModule;
-
-    @EJB
-    private MovementServiceBean movementModule;
-
-    @EJB
-    private SpatialService spatialModule;
-
-    @EJB
-    private USMService usmService;
+    private @EJB ReportRepository repository;
+    private @EJB AuditService auditService;
+    private @EJB AssetServiceBean assetModule;
+    private @EJB MovementServiceBean movementModule;
+    private @EJB SpatialService spatialModule;
+    private @EJB USMService usmService;
 
     @Override
     @Transactional
@@ -58,100 +45,65 @@ public class VmsServiceBean implements VmsService {
     public VmsDTO getVmsDataByReportId(final String username, final String scopeName, final Long id, final List<AreaIdentifierType> areaRestrictions) throws ReportingServiceException {
 
         log.debug("[START] getVmsDataByReportId({}, {}, {})", username, scopeName, id);
-
         Report reportByReportId = repository.findReportByReportId(id, username, scopeName);
 
         if (reportByReportId == null) {
-
             String error = MessageFormatter.arrayFormat("No report found with id {}", new Object[]{id}).getMessage();
-
             log.error(error);
-
             throw new ReportingServiceException(error);
-
         }
 
         VmsDTO vmsDto = getVmsData(reportByReportId, areaRestrictions);
-
         reportByReportId.updateExecutionLog(username);
-
         log.debug("[END] getVmsDataByReportId(...)");
-
         return vmsDto;
-
     }
 
     private VmsDTO getVmsData(Report report, List<AreaIdentifierType> areaRestrictions) throws ReportingServiceException {
 
         try {
-
             Map<String, Asset> assetMap;
-
             FilterProcessor processor = new FilterProcessor(report.getFilters());
-
             if (areaRestrictions != null) {
                 processor.getScopeRestrictionAreaIdentifierList().addAll(areaRestrictions);
             }
-
             addAreaCriteriaToProcessor(processor);
-
             Collection<MovementMapResponseType> movementMap;
-
             Map<String, MovementMapResponseType> responseTypeMap;
-
             log.debug("Running report {} assets or asset groups.", processor.hasAssetsOrAssetGroups() ? "has" : "doesn't have");
 
             if (processor.hasAssetsOrAssetGroups()) {
-
                 assetMap = assetModule.getAssetMap(processor);
-
                 processor.getMovementListCriteria().addAll(ExtMovementMessageMapper.movementListCriteria(assetMap.keySet()));
-
                 movementMap = movementModule.getMovement(processor);
-
             } else {
-
                 responseTypeMap = movementModule.getMovementMap(processor);
-
                 Set<String> assetGuids = responseTypeMap.keySet();
-
                 movementMap = responseTypeMap.values();
-
                 processor.getAssetListCriteriaPairs().addAll(ExtAssetMessageMapper.assetCriteria(assetGuids));
-
                 assetMap = assetModule.getAssetMap(processor);
             }
-
             return new VmsDTO(assetMap, movementMap);
 
         } catch (ProcessorException e) {
-
             String error = "Error while processing reporting filters";
-
             log.error(error, e);
-
             throw new ReportingServiceException(error, e);
-
         }
-
     }
 
     @Override
     public VmsDTO getVmsDataBy(final eu.europa.ec.fisheries.uvms.reporting.model.vms.Report report, final List<AreaIdentifierType> areaRestrictions) throws ReportingServiceException {
 
         VmsDTO vmsData = getVmsData(ReportMapperV2.INSTANCE.reportDtoToReport(report), areaRestrictions);
-
         auditService.sendAuditReport(AuditActionEnum.EXECUTE, report.getName());
-
         return vmsData;
-
     }
 
     private void addAreaCriteriaToProcessor(FilterProcessor processor) throws ReportingServiceException {
 
         final Set<AreaIdentifierType> areaIdentifierList = processor.getAreaIdentifierList();
         final Set<AreaIdentifierType> scopeAreaIdentifierList = processor.getScopeRestrictionAreaIdentifierList();
-
         try {
             if (presentAreasToFilter(areaIdentifierList, scopeAreaIdentifierList)) {
                 String areaWkt = spatialModule.getFilterArea(scopeAreaIdentifierList, areaIdentifierList);
@@ -159,9 +111,7 @@ public class VmsServiceBean implements VmsService {
             }
         } catch (ReportingServiceException e) {
             String error = "Exception during retrieving filter area";
-
             log.error(error, e);
-
             throw new ReportingServiceException(error, e);
         }
     }
