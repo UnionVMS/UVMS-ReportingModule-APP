92a2e2571f54442f958bac513b79a7efb86f6418
EMRINALGR <EMRINALGR@mycompany.com>
2016-03-22 14:30:35 +0000

    Adding timestamp as now  and fixing bug on run report without same
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Reporting/APP@10002 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/reporting/rest/resources/ReportingResource.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/reporting/rest/resources/ReportingResource.java
@@ -325,10 +325,10 @@ public class ReportingResource extends UnionVMSResource {
         try {
             Map additionalProperties = (Map) report.getAdditionalProperties().get(ADDITIONAL_PROPERTIES);
 
-            final VelocityType velocityType =
-                    VelocityType.valueOf(additionalProperties.get(SPEED_UNIT.toUpperCase()).toString());
-            final LengthType lengthType = 
-                    LengthType.valueOf(additionalProperties.get(DISTANCE_UNIT.toUpperCase()).toString());
+            final VelocityType velocityType = 
+                    VelocityType.valueOf(additionalProperties.get(SPEED_UNIT).toString().toUpperCase());
+            final LengthType lengthType =
+                    LengthType.valueOf(additionalProperties.get(DISTANCE_UNIT).toString().toUpperCase());
 
             final DisplayFormat displayFormat = new DisplayFormat(velocityType, lengthType);
             final List<AreaIdentifierType> areaRestrictions = getRestrictionAreas(username, scopeName, roleName);
