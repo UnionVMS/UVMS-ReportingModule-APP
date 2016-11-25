b1655756ebc4f74072c54ca51474f259d340e755
AdityaPadhy <aditya.padhy123@gmail.com>
2016-11-25 15:49:47 +0100

    UNINONVMS-1568 , Updating Report execution to support Fishing activity and Trips

---
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/reporting/message/service/ActivityModuleSenderBean.java
@@ -0,0 +1,7 @@
+package eu.europa.ec.fisheries.uvms.reporting.message.service;
+
+/**
+ * Created by padhyad on 11/23/2016.
+ */
+public class ActivityModuleSenderBean {
+}
---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/ActivityService.java
@@ -0,0 +1,7 @@
+package eu.europa.ec.fisheries.uvms.reporting.service.bean;
+
+/**
+ * Created by padhyad on 11/23/2016.
+ */
+public class ActivityService {
+}
