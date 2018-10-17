96b52f887bdd3b0e7242e32b261afd36af200db0
Kasim Gul <kasimgul@gmail.com>
2018-10-17 15:04:35 +0200

    REST call to MovementRules

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/impl/RulesEventServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/impl/RulesEventServiceBean.java
@@ -9,51 +9,42 @@ details. You should have received a copy of the GNU General Public License along
 
  */
 
-
 package eu.europa.ec.fisheries.uvms.reporting.service.bean.impl;
 
+import eu.europa.ec.fisheries.schema.rules.module.v1.GetTicketsAndRulesByMovementsRequest;
 import eu.europa.ec.fisheries.schema.rules.module.v1.GetTicketsAndRulesByMovementsResponse;
 import eu.europa.ec.fisheries.schema.rules.ticketrule.v1.TicketAndRuleType;
-import eu.europa.ec.fisheries.uvms.commons.message.api.MessageException;
-import eu.europa.ec.fisheries.uvms.reporting.message.service.ReportingModuleReceiverBean;
-import eu.europa.ec.fisheries.uvms.reporting.message.service.RulesProducerBean;
-import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingServiceException;
 import eu.europa.ec.fisheries.uvms.reporting.service.bean.RulesEventService;
-import eu.europa.ec.fisheries.uvms.rules.model.exception.RulesFaultException;
-import eu.europa.ec.fisheries.uvms.rules.model.exception.RulesModelMapperException;
-import eu.europa.ec.fisheries.uvms.rules.model.mapper.RulesModuleRequestMapper;
-import eu.europa.ec.fisheries.uvms.rules.model.mapper.RulesModuleResponseMapper;
-import java.util.List;
-import javax.ejb.EJB;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ejb.Local;
 import javax.ejb.Stateless;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.TextMessage;
-import lombok.extern.slf4j.Slf4j;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Stateless
 @Local(RulesEventService.class)
 @Slf4j
 public class RulesEventServiceBean implements RulesEventService {
 
-    @EJB
-    private RulesProducerBean producer;
-
-    @EJB
-    private ReportingModuleReceiverBean consumer;
-
     @Override
-    public List<TicketAndRuleType> GetAlarmsForMovements(List<String> movementId) throws ReportingServiceException {
-        try {
-            String request = RulesModuleRequestMapper.createGetTicketsAndRulesByMovementsRequest(movementId);
-            String correlationId = producer.sendModuleMessage(request, consumer.getDestination());
-            Message message = consumer.getMessage(correlationId, TextMessage.class);
-            GetTicketsAndRulesByMovementsResponse response = RulesModuleResponseMapper.mapToGetTicketsAndRulesByMovementsFromResponse((TextMessage) message);
-            return response != null ? response.getTicketsAndRules() : null;
-        } catch (MessageException | JMSException | RulesModelMapperException | RulesFaultException e) {
-            throw new ReportingServiceException(e);
-        }
-    }
+    public List<TicketAndRuleType> GetAlarmsForMovements(List<String> movementId) {
 
+        GetTicketsAndRulesByMovementsRequest request = new GetTicketsAndRulesByMovementsRequest();
+        request.getMovementGuids().addAll(movementId);
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("localhost:8080/unionvms/movement-rules/rest/internal/");
+
+        GetTicketsAndRulesByMovementsResponse response = target
+                .path("tickets-and-rules-by-movement")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(request), GetTicketsAndRulesByMovementsResponse.class);
+
+        return response != null ? response.getTicketsAndRules() : null;
+    }
 }
