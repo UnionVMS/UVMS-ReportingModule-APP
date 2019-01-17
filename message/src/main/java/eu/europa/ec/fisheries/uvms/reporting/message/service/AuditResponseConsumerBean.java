72c5a59e85b1b1d6f13da54d42e28e1b642cae7c
ANKOVI <fevamko@yahoo.com>
2019-01-17 12:24:29 +0100

    Update

--- message/src/main/java/eu/europa/ec/fisheries/uvms/reporting/message/service/AuditResponseConsumerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/reporting/message/service/AuditResponseConsumerBean.java
@@ -3,6 +3,11 @@ package eu.europa.ec.fisheries.uvms.reporting.message.service;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
 import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractConsumer;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+@Stateless
+@LocalBean
 public class AuditResponseConsumerBean extends AbstractConsumer {
     @Override
     public String getDestinationName() {
