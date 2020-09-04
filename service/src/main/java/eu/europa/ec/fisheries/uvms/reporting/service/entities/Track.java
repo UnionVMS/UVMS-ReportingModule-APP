b80e4f601c33c81cb804e831ab0483570e188bde
kwstaspgr <constantinos.papatheodorou@sword-group.com>
2020-09-04 11:42:10 +0300

    [UNIONVMS-4657-data-model] - Create entities based on reporting response model

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/entities/Track.java
@@ -0,0 +1,74 @@
+/*
+﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+© European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.europa.ec.fisheries.uvms.reporting.service.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.vividsolutions.jts.geom.MultiPoint;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.opengis.geometry.primitive.Point;
+
+@Entity
+@Table(name = "track")
+@Data
+@NoArgsConstructor
+public class Track {
+
+    @Id
+    @SequenceGenerator(name = "track_seq", sequenceName = "track_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "track_seq")
+    private Long id;
+
+    @Column(name = "guid")
+    private String guid;
+
+    @Type(type = "org.hibernate.spatial.GeometryType")
+    @Column(name = "nearest_point_coordinates", columnDefinition = "Geometry")
+    private Point nearestPoint;
+
+    @Type(type = "org.hibernate.spatial.GeometryType")
+    @Column(name = "extent_coordinates", columnDefinition = "Geometry")
+    private MultiPoint extent;
+
+    @Column(name = "duration")
+    private Double duration;
+
+    @Column(name = "distance")
+    private Double distance;
+
+    @Column(name = "total_time_at_sea")
+    private Double totalTimeAtSea;
+
+    @Column(name = "cfr")
+    private String cfr;
+
+    @Column(name = "ircs")
+    private String ircs;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "ext_mark")
+    private String externalMakrking;
+
+    @Column(name = "country_code")
+    private String countryCode;
+
+}
