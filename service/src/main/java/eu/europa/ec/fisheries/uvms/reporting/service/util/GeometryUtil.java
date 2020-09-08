c2fbef6505195f6790cfcdf2aaa45be342c8b472
lsotiriadis <leontion.sotiriadis@sword-group.com>
2020-09-08 17:18:17 +0300

    [UNIONVMS-4659] WIP
    Signed-off-by: lsotiriadis <leontion.sotiriadis@sword-group.com>

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/util/GeometryUtil.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/util/GeometryUtil.java
@@ -10,11 +10,17 @@ details. You should have received a copy of the GNU General Public License along
  */
 package eu.europa.ec.fisheries.uvms.reporting.service.util;
 
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
 
 public class GeometryUtil {
+
+    private static final GeometryFactory FACTORY = new GeometryFactory();
+    static final int SRID = 4326;
 
     private GeometryUtil() {
         super();
@@ -29,4 +35,25 @@ public class GeometryUtil {
         return geometry;
     }
 
+    /**
+     * Returns a LineString for the given wkt with the Geometry coordinates
+     * @param wkt The wkt String
+     * @return LineString object
+     * @throws ParseException Thrown if string has errors
+     */
+    public static LineString toLineString(final String wkt) throws ParseException{
+        return getLineString(toGeometry(wkt).getCoordinates());
+    }
+
+    /**
+     * Returns a LineString for insertion in database
+     *
+     * @param sequence Coordinate array
+     * @return LineString object
+     */
+    public static LineString getLineString(Coordinate[] sequence) {
+        LineString lineString = FACTORY.createLineString(sequence);
+        lineString.setSRID(SRID);
+        return lineString;
+    }
 }
