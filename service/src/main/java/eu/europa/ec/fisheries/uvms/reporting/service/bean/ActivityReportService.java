9f52677b4cc268fa9c0121ba6565f51e3ad74681
kwstaspgr <constantinos.papatheodorou@sword-group.com>
2020-11-04 18:08:29 +0200

    fixes after integration - incoming topic messages and fixes for saving movement, activity and related tables

--- service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/ActivityReportService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/reporting/service/bean/ActivityReportService.java
@@ -1,7 +1,11 @@
 package eu.europa.ec.fisheries.uvms.reporting.service.bean;
 
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.ForwardReportToSubscriptionRequest;
 import un.unece.uncefact.data.standard.fluxfareportmessage._3.FLUXFAReportMessage;
 
 public interface ActivityReportService {
     void createActivitiesAndTrips(FLUXFAReportMessage activityData);
+
+    void createActivitiesAndTrips(ForwardReportToSubscriptionRequest activityData);
+
 }
